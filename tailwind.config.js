ad2e00faeafc195236c0c904d38bf6e2eadcd5be
PeriYumYum <perilin124@gmail.com>
2023-08-08 14:26:18 +0800

    Setup Tailwind css, add CDN link of font-awesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'weather-primary': '#7c3aed',
+        'weather-secondary': '#a78bfa',
+      },
+    },
     fontFamily: {
       Roboto: ['Roboto, sans-serif'],
+    },
+    container: {
+      padding: '2rem',
+      center: true,
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
     },
   },
   plugins: [],
