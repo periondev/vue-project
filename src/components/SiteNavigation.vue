96ce877f78504fb645e5773f02c0226c53ffc80d
PeriYumYum <perilin124@gmail.com>
2023-08-09 15:49:28 +0800

    Create SiteNavigation

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="sticky top-0 shadow-lg bg-weather-primary">
+    <nav
+      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-cloud-sun text-2xl"></i>
+          <p class="text-2xl">The Local Weather</p>
+        </div>
+      </RouterLink>
+      <div class="flex flex-1 gap-3 justify-end">
+        <i
+          class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+        ></i>
+        <i
+          class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+        ></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
