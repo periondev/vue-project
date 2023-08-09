39a315143393db1f4404e8d0fdd76626d77eec7b
PeriYumYum <perilin124@gmail.com>
2023-08-09 23:58:45 +0800

    Create BaseModel, add website info

---
+++ src/components/BaseModel.vue
@@ -0,0 +1,35 @@
+<template>
+  <Transition name="model-outer">
+    <div
+      v-show="modelActive"
+      class="flex absolute top-0 left-0 w-full h-screen justify-center px-8 bg-black bg-opacity-30"
+    >
+      <Transition name="model-inner">
+        <div
+          v-if="modelActive"
+          class="p-4 mt-32 max-w-screen-md self-start bg-white"
+        >
+          <slot />
+          <button
+            class="mt-8 py-2 px-6 text-white bg-weather-primary"
+            @click="$emit('close-model')"
+          >
+            關閉說明
+          </button>
+        </div>
+      </Transition>
+    </div>
+  </Transition>
+</template>
+
+<script setup lang="ts">
+defineEmits(['close-model']);
+defineProps({
+  modelActive: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style scoped></style>
--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -6,21 +6,49 @@
       <RouterLink :to="{ name: 'home' }">
         <div class="flex items-center gap-3">
           <i class="fa-solid fa-cloud-sun text-2xl"></i>
-          <p class="text-2xl">The Local Weather</p>
+          <p class="text-2xl">天氣預報</p>
         </div>
       </RouterLink>
       <div class="flex flex-1 gap-3 justify-end">
         <i
           class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+          @click="toggleBaseModel"
         ></i>
         <i
           class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
         ></i>
       </div>
+      <BaseModel :modelActive="modelActive" @close-model="toggleBaseModel">
+        <div class="text-black">
+          <h1 class="text-2xl mb-2">關於本網頁：</h1>
+          <p class="mb-4">
+            "天氣預報"讓您可以追蹤所選城市的當下和未來天氣狀況。
+          </p>
+          <h2 class="text-2xl">使用方法：</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>在搜索欄中輸入城市名稱，搜索您想要的城市。</li>
+            <li>在搜索結果中選擇一個城市，這將帶您到該城市的當前天氣頁面。</li>
+            <li>
+              只要點擊頁面右上角的"+"圖示，就可以把該城市保存到主頁並追蹤該城市，方便之後隨時查看。
+            </li>
+          </ol>
+          <h2 class="text-2xl">取消追蹤城市:</h2>
+          <p>
+            如果您不想再追蹤某個城市，只需在主頁中選擇該城市，並點擊位在頁面底部刪除該城市的選項。
+          </p>
+        </div>
+      </BaseModel>
     </nav>
   </header>
 </template>
 
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
+import BaseModel from './BaseModel.vue';
+import { ref } from 'vue';
+
+const modelActive = ref(false);
+const toggleBaseModel = () => {
+  modelActive.value = !modelActive.value;
+};
 </script>
