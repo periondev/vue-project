555b0469546ac7092a82ef170a08ee3b4af9bae2
PeriYumYum <perilin124@gmail.com>
2023-08-11 10:53:03 +0800

    Create search bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <main>
- 
+  <main class="container">
+    <div class="relative my-6 flex w-full flex-wrap items-stretch">
+      <input
+        type="text"
+        class="bg-white h-12 w-full px-4 rounded-lg focus:outline-none hover:cursor-pointer"
+        name="search bar"
+        aria-label="搜尋框"
+        placeholder="選擇縣市"
+      />
+      <span class="absolute top-3 right-4 border-l pl-4"
+        ><i
+          class="fa fa-search text-gray-600 hover:text-weather-secondary hover:cursor-pointer"
+        ></i
+      ></span>
+    </div>
   </main>
 </template>
+
+<script setup lang="ts"></script>
