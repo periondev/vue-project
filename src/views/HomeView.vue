8dbf454e90230eb61aeb36bcdcd037aff781b3ef
PeriYumYum <perilin124@gmail.com>
2023-08-13 22:29:18 +0800

    Create SearchBar component, add dropdown menu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,9 @@
 <template>
   <main class="container">
-    <div class="relative my-6 flex w-full flex-wrap items-stretch">
-      <input
-        type="text"
-        class="bg-white h-12 w-full px-4 rounded-lg focus:outline-none hover:cursor-pointer"
-        name="search bar"
-        aria-label="搜尋框"
-        placeholder="選擇縣市"
-      />
-      <span class="absolute top-3 right-4 border-l pl-4"
-        ><i
-          class="fa fa-search text-gray-600 hover:text-weather-secondary hover:cursor-pointer"
-        ></i
-      ></span>
-    </div>
+    <SearchBar />
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SearchBar from '@/components/SearchBar.vue';
+</script>
