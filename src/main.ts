593a6575a6783e613322d5780912e1868a08b667
PeriYumYum <perilin124@gmail.com>
2023-09-03 11:27:36 +0800

    Add pinia-plugin-persistedstate

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import './assets/tailwind.css';
 
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
+const pinia = createPinia();
 
+app.use(pinia.use(piniaPluginPersistedstate));
 app.use(router);
 
 app.mount('#app');
