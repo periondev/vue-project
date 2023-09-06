00d7427f264ff869028fd87a61c980d381da184e
PeriYumYum <perilin124@gmail.com>
2023-09-06 14:17:20 +0800

    Update data and code refactoring

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,41 @@
-import { defineStore } from 'pinia';
+import { defineStore, getActivePinia } from 'pinia';
 import type { History } from '@/types';
 
 export const useLocationStore = defineStore('store', {
-  state: () => {
-    return {
-      historyList: <History[]>[],
-    };
+  // 定義狀態
+  state: () => ({
+    history: <History[]>[],
+  }),
+  // 定義方法
+  actions: {
+    // 添加一筆紀錄到狀態中
+    addHistory(city: string, region: string) {
+      // 檢查是否已經存在相同的紀錄
+      const exist = this.history.find(
+        (r) => r.city === city && r.region === region
+      );
+      if (!exist) {
+        // 如果不存在，則將紀錄推入陣列的開頭
+        this.history.unshift({ city, region });
+        // 如果紀錄超過7筆，則移除最後一筆
+        if (this.history.length > 7) {
+          this.history.pop();
+        }
+      }
+    },
+    // 重設全局store狀態的方法
+    resetAllStores() {
+      const activepinia = getActivePinia();
+      if (activepinia) {
+        Object.entries(activepinia.state.value).forEach(
+          ([storeName, state]) => {
+            const storeDefinition = defineStore(storeName, state);
+            const store = storeDefinition(activepinia);
+            store.$reset();
+          }
+        );
+      }
+    },
   },
   persist: true,
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,27 @@
 <template>
   <main class="container mx-auto p-4">
     <div class="flex justify-center min-w-36 items-center space-x-4 my-4">
-      <select v-model="selectedCity" @change="updateRegions">
-        <option v-for="city in cities" :value="city">{{ city }}</option>
+      <select
+        title="select city"
+        v-model="selectedCity"
+        @change="updateRegions"
+      >
+        <option v-for="city in cities" :key="city.dataId" :value="city.name">
+          {{ city.name }}
+        </option>
       </select>
-      <select v-model="selectedRegion">
-        <option v-for="region in regions" :value="region">{{ region }}</option>
+      <select title="select region" v-model="selectedRegion">
+        <option v-for="region in regions" :key="region" :value="region">
+          {{ region }}
+        </option>
       </select>
-      <button @click="addToHistory">確認</button>
+      <button type="submit" @click="confirm">確認</button>
     </div>
     <ul>
-      <li
-        v-for="(history, index) in historyList"
-        :key="index"
-        @click="updateFromHistory(history)"
-      >
-        {{ history.city }} - {{ history.region }}
+      <li v-for="(history, index) in historyList" :key="index">
+        <span @click="updateFromHistory(history)" class="cursor-pointer"
+          >{{ history.city }} - {{ history.region }}</span
+        >
       </li>
     </ul>
   </main>
@@ -26,58 +32,53 @@ import { defineComponent, ref } from 'vue';
 import locationData from '../location.json';
 import { useLocationStore } from '@/store/store';
 import type { History } from '@/types';
+console.log(locationData);
 
 export default defineComponent({
   name: 'Home',
   setup() {
-    const cities = locationData.locations.map((location) => location.name);
-    const selectedCity = ref(cities[0]);
-    const regions =
-      locationData.locations.find(
-        (location) => location.name === selectedCity.value
-      )?.regions || [];
-    const selectedRegion = ref(regions[0]);
-    //const historyList = ref<History[]>([]);
+    // 讀取location全局狀態
     const locationStore = useLocationStore();
+    // 視需要重設全局store狀態
+    //locationStore.resetAllStores();
 
-    //BUG:再點擊宜蘭縣無法調出鄉鎮市區
-    function updateRegions() {
-      regions.length = 0;
-      const selectedLocation = locationData.locations.find(
-        (location) => location.name === selectedCity.value
-      );
-      if (selectedLocation) {
-        regions.push(...selectedLocation.regions);
-        selectedRegion.value = regions[0];
+    // 從locationData讀取所有縣市資料
+    const cities = ref(locationData);
+    // 鄉鎮市區列表
+    const regions = ref(cities.value[0].regions);
+    // 選擇的縣市
+    const selectedCity = ref(cities.value[0].name);
+    // 選擇的鄉鎮市區
+    const selectedRegion = ref(cities.value[0].regions[0]);
+    // 根據選擇的縣市更新鄉鎮市區
+    const updateRegions = () => {
+      const city = cities.value.find((c) => c.name === selectedCity.value);
+      if (city) {
+        regions.value = city.regions;
+        selectedRegion.value = city.regions[0];
       }
-    }
+    };
 
-    // 實現數據持久化
-    function addToHistory() {
-      locationStore.historyList.unshift({
-        city: selectedCity.value,
-        region: selectedRegion.value,
-      });
-      if (locationStore.historyList.length > 8) {
-        locationStore.historyList.pop();
-      }
-    }
-
-    function updateFromHistory(history: History) {
+    // 點擊確認按鈕，將選擇的縣市和鄉鎮市區加入歷史紀錄
+    const confirm = () => {
+      locationStore.addHistory(selectedCity.value, selectedRegion.value);
+    };
+    // 點擊歷史紀錄欄位，更新選擇的縣市和鄉鎮市區
+    const updateFromHistory = (history: History) => {
       selectedCity.value = history.city;
       updateRegions();
       selectedRegion.value = history.region;
-    }
+    };
 
     return {
       cities,
-      selectedCity,
       regions,
+      selectedCity,
       selectedRegion,
-      addToHistory,
-      historyList: locationStore.historyList,
-      updateFromHistory,
       updateRegions,
+      confirm,
+      historyList: locationStore.history,
+      updateFromHistory,
     };
   },
 });
