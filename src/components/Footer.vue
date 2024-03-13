67613de86efb705ffdeb351cddd6451106af9cf7
PeriYumYum <perilin124@gmail.com>
2024-03-13 22:41:55 +0800

    Update links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,18 @@
 <template>
-  <footer class="container mx-auto p-4 mt-8 text-center text-white/50">
-    <p>Developed by Peri🐚 © 2024</p>
+  <footer class="container mx-auto p-4 mt-8 text-center text-white/60">
+    <p>
+      Developed by
+      <a href="https://github.com/periondev" title="github" target="_blank"
+        >Peri🐚</a
+      >
+      © 2024
+    </p>
+    <a
+      href="https://www.flaticon.com/free-icons/weather"
+      title="weather icons"
+      target="_blank"
+      class="text-xs"
+      >Weather icons created by fjstudio - Flaticon</a
+    >
   </footer>
 </template>
