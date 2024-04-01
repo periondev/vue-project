6e7e701f871b9c12a688a0fa7056dd6197c0817e
PeriYumYum <perilin124@gmail.com>
2024-04-01 16:46:13 +0800

    Refactoring code

--- src/store/history.ts
+++ src/store/history.ts
@@ -1,11 +1,49 @@
+import { ref } from 'vue';
 import { defineStore } from 'pinia';
+import { useCurrentWeather } from '@/store/currentWeather';
+import { useWeeklyWeather } from '@/store/weeklyWeather';
 import type { History } from '@/types';
+import locationData from '../location.json';
+const cities = locationData;
 
 export const useHistory = defineStore('history', {
   state: () => ({
     history: <History[]>[],
+    cities: ref(cities),
+    regions: ref(cities[0].regions),
+    selectedCity: ref(cities[0].name),
+    selectedRegion: ref(cities[0].regions[0]),
+    selectedCityDataId: ref(cities[0].dataId),
   }),
   actions: {
+    setUpdateRegions(city: string) {
+      // 根據選擇的縣市更新鄉鎮
+      const setCity = cities.find((c) => c.name === city);
+      if (setCity) {
+        this.regions = setCity.regions;
+        this.selectedRegion = setCity.regions[0];
+        this.selectedCityDataId = setCity.dataId;
+      }
+    },
+    setSelectedLocation(city: string, region: string, dataId: string[]) {
+      this.selectedCity = city;
+      this.selectedRegion = region;
+      this.selectedCityDataId = dataId;
+    },
+    onFetchWeather(city: string, region: string, dataId: string[]) {
+      const currentWeather = useCurrentWeather();
+      const weeklyWeather = useWeeklyWeather();
+      weeklyWeather.fetchWeather(city, region, dataId);
+      currentWeather.fetchWeather(city, region, dataId);
+    },
+    // 取得上次查詢地區的天氣資料，否則取得初始地區天氣資料
+    initialFetchWeather() {
+      this.onFetchWeather(
+        this.selectedCity,
+        this.selectedRegion,
+        this.selectedCityDataId
+      );
+    },
     addHistory(city: string, region: string, dataId: string[]) {
       // 檢查紀錄是否已存在相同的地區
       const exist = this.history.find(
