4d7893023f524d1f1f05ff605bab1982cebd69b8
PeriYumYum <perilin124@gmail.com>
2024-04-01 21:46:43 +0800

    Extracting location history template and functions from LocationMenu, import History component and use intial fetching function in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <template>
   <main class="container mx-auto px-4 mt-14 tracking-wide">
     <LocationMenu />
+    <History />
     <CurrentDisplay />
     <WeeklyDisplay />
   </main>
 </template>
 
 <script setup lang="ts">
+import { useHistory } from '@/store/history';
 import LocationMenu from '@/components/LocationMenu.vue';
+import History from '@/components/History.vue';
 import CurrentDisplay from '@/components/CurrentDisplay.vue';
 import WeeklyDisplay from '@/components/WeeklyDisplay.vue';
+const historyStore = useHistory();
+historyStore.initialFetchWeather(); // 網頁取得初始天氣資料
 </script>
