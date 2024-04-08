414fb8d2aa3d4896b0fb84b0a7d3d81a0f7ddc13
PeriYumYum <perilin124@gmail.com>
2024-04-08 14:11:58 +0800

    Update links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,14 @@
 <template>
   <footer class="container mx-auto p-4 mt-8 text-center text-white/60">
+    <p class="mb-3">
+      <RouterLink to="/" class="underline hover:font-bold">
+        {{ $t('HomeTitle') }}
+      </RouterLink>
+      {{ '  |  ' }}
+      <RouterLink to="/about" class="underline hover:font-bold">
+        {{ $t('AboutTitle') }}
+      </RouterLink>
+    </p>
     <p>
       Developed by
       <a href="https://github.com/periondev" title="github" target="_blank"
@@ -7,7 +16,6 @@
       >
       © 2024
     </p>
-    <RouterLink to="/about"><p>About</p></RouterLink>
     <a
       href="https://www.flaticon.com/free-icons/weather"
       title="weather icons"
