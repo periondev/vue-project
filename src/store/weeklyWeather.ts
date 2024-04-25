684b11e4c04bfa0bbf940fcb3e5f9e0696bc8017
PeriYumYum <perilin124@gmail.com>
2024-04-25 23:39:54 +0800

    Create cheartElements to save chart data

--- src/store/weeklyWeather.ts
+++ src/store/weeklyWeather.ts
@@ -1,11 +1,11 @@
 import { defineStore } from 'pinia';
-import type { WeatherData, WeeklyElements } from '@/types';
+import type { ChartElements, WeeklyElements } from '@/types';
 import axios from 'axios';
 
 export const useWeeklyWeather = defineStore('weeklyWeather', {
   state: () => ({
-    weatherData: {} as WeatherData,
     elements: <WeeklyElements[]>[], // 一週天氣元素按照日期排序集合儲存於陣列中
+    chartElements: {} as ChartElements, // 一週折線圖數據 Chart data
   }),
   actions: {
     async fetchWeather(city: string, region: string, dataId: string[]) {
@@ -23,8 +23,6 @@ export const useWeeklyWeather = defineStore('weeklyWeather', {
         // 由API獲取的指定天氣因子資料存進data
         const data =
           response.data.records.locations[0].location[0].weatherElement;
-        this.weatherData.cityName = city;
-        this.weatherData.regionName = region;
 
         if (data) {
           const pop12h = data[0].time;
@@ -43,28 +41,39 @@ export const useWeeklyWeather = defineStore('weeklyWeather', {
           const wxArr = getValue(wx); // 天氣現象
 
           // 排除陣列第一筆資料:因查詢時間區段跨夜(After 18:00)造成第一筆為過時資料
-          dateArr.length > 14 ? dateArr.shift() : dateArr;
-          popArr.length > 14 ? popArr.shift() : popArr;
-          tArr.length > 14 ? tArr.shift() : tArr;
-          rhArr.length > 14 ? rhArr.shift() : rhArr;
-          wxArr.length > 14 ? wxArr.shift() : wxArr;
+          [dateArr, popArr, tArr, rhArr, wxArr].forEach(
+            (arr) => arr.length > 14 && arr.shift()
+          );
+
+          // 簡化日期:月/日、格式化日期
+          const date = dateArr.map((d: string) =>
+            d.split(' ')[0].split('-').slice(1).join('/')
+          );
+          const formatedDate = dateArr.map((d: string) => {
+            return new Date(d);
+          });
 
           // 使用迭代方法將陣列轉換為物件儲存於陣列中
           for (let i = 0; i < 7; i++) {
-            // 格式化週、日期
-            const dayOfWeek = new Date(dateArr[i * 2]);
-            const date = dateArr.map((d: string) =>
-              d.split(' ')[0].split('-').slice(1).join('/')
-            );
             this.elements[i] = {
-              dayOfWeek: dayOfWeek,
+              dayOfWeek: formatedDate[i * 2],
               date: date[i * 2],
               pop: [popArr[i * 2], popArr[i * 2 + 1]],
-              t: [tArr[i * 2], tArr[i * 2 + 1]],
+              temp: [tArr[i * 2], tArr[i * 2 + 1]],
               rh: [rhArr[i * 2], rhArr[i * 2 + 1]],
               wx: [wxArr[i * 2], wxArr[i * 2 + 1]],
             };
           }
+          // 折線圖數據
+          this.chartElements = {
+            date: date.filter((_: any, i: number) => i % 2 === 0),
+            tempDay: tArr
+              .filter((_: any, i: number) => i % 2 === 0)
+              .map((str: string) => parseInt(str)),
+            tempNight: tArr
+              .filter((_: any, i: number) => i % 2 === 1)
+              .map((str: string) => parseInt(str)),
+          };
         }
       } catch (error) {
         console.error(error);
