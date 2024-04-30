5e21d85bc1ae96d1a14d8104c1648f0e4424420f
PeriYumYum <perilin124@gmail.com>
2024-04-30 11:20:34 +0800

    Create CurrentChart.vue

---
+++ src/components/CurrentChart.vue
@@ -0,0 +1,89 @@
+<template>
+  <Line :options="chartOptions" :data="chartData" />
+</template>
+
+<script setup lang="ts">
+import { toRefs, computed } from 'vue';
+import { useCurrentWeather } from '@/store/currentWeather';
+import { Line } from 'vue-chartjs';
+import i18n from '../utils/vue-i18n';
+const { t } = i18n.global;
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+// Chart config
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+// Chart data
+const store = useCurrentWeather();
+const { currentChartData } = toRefs(store);
+
+const chartData = computed(() => ({
+  labels: currentChartData.value.date,
+  datasets: [
+    {
+      label: '溫度',
+      data: currentChartData.value.temp,
+      backgroundColor: '#2dd4bf',
+      borderColor: '#2dd4bf',
+      hoverRadius: 6,
+    },
+    {
+      label: '體感溫度',
+      data: currentChartData.value.at,
+      backgroundColor: '#fecdd3',
+      borderColor: '#fecdd3',
+      hoverRadius: 6,
+    },
+  ],
+}));
+const chartOptions = computed(() => ({
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      labels: {
+        boxWidth: 24,
+        color: '#fff',
+      },
+    },
+  },
+  scales: {
+    x: {
+      grid: {
+        color: '#525252',
+      },
+      ticks: {
+        color: '#fff',
+      },
+    },
+    y: {
+      grid: {
+        color: '#525252',
+      },
+      ticks: {
+        color: '#fff',
+      },
+      suggestedMax: 32,
+      suggestedMin: 20,
+    },
+  },
+}));
+</script>
