2765ba21d672ea0cf92b1ad7b5848533b6902469
PeriYumYum <perilin124@gmail.com>
2024-04-30 15:04:17 +0800

    Create currentChartData

--- src/store/currentWeather.ts
+++ src/store/currentWeather.ts
@@ -1,10 +1,11 @@
 import { defineStore } from 'pinia';
-import type { CurrentElements } from '@/types';
+import type { CurrentChartData, CurrentElements } from '@/types';
 import axios from 'axios';
 
 export const useCurrentWeather = defineStore('currentWeather', {
   state: () => ({
     currentData: {} as CurrentElements,
+    currentChartData: {} as CurrentChartData, // 折線圖數據 Chart data
   }),
   actions: {
     async fetchWeather(city: string, region: string, dataId: string[]) {
@@ -15,7 +16,7 @@ export const useCurrentWeather = defineStore('currentWeather', {
             params: {
               Authorization: import.meta.env.VITE_API,
               locationName: region,
-              elementName: 'Wx,T,RH,CI,PoP6h,WS',
+              elementName: 'Wx,AT,T,RH,CI,PoP6h,WS',
             },
           }
         );
@@ -25,11 +26,11 @@ export const useCurrentWeather = defineStore('currentWeather', {
 
         if (dataNow) {
           const wx = dataNow[0].time[1].elementValue[0].value; // Wx 天氣現象
-          const t = dataNow[1].time[1].elementValue[0].value; // T 平均溫度
-          const rh = dataNow[2].time[1].elementValue[0].value; // RH 相對濕度
-          const ci = dataNow[3].time[1].elementValue[1].value; // CI 舒適度指數, 舒適度文字描述
-          const pop6h = dataNow[4].time[1].elementValue[0].value; // PoP6h 6小時降雨機率
-          const ws = dataNow[5].time[1].elementValue[0].value; // WS 風速 公尺/秒
+          const t = dataNow[2].time[1].elementValue[0].value; // T 平均溫度
+          const rh = dataNow[3].time[1].elementValue[0].value; // RH 相對濕度
+          const ci = dataNow[4].time[1].elementValue[1].value; // CI 舒適度指數, 舒適度文字描述
+          const pop6h = dataNow[5].time[1].elementValue[0].value; // PoP6h 6小時降雨機率
+          const ws = dataNow[6].time[1].elementValue[0].value; // WS 風速 公尺/秒
 
           // 將資料存進 currentData 物件
           this.currentData = {
@@ -41,6 +42,29 @@ export const useCurrentWeather = defineStore('currentWeather', {
             wx: wx,
             ci: ci,
             ws: ws,
+          };
+
+          const TCollection = [...dataNow[2].time.slice(0, 24)];
+          const ATCollection = [...dataNow[1].time.slice(0, 24)];
+
+          // 處理日期方法
+          const getDate = (str: string) =>
+            str.split(' ')[0].split('-').slice(1).join('/');
+          const getTime = (str: string) => str.split(' ')[1].slice(0, 5);
+
+          const dateArr = TCollection.map((el) => [
+            getDate(el.dataTime),
+            getTime(el.dataTime),
+          ]);
+
+          const tArr = TCollection.map((el: any) => el.elementValue[0].value);
+          const atArr = ATCollection.map((el: any) => el.elementValue[0].value);
+
+          // 將資料存進 currentChartData 物件
+          this.currentChartData = {
+            date: dateArr,
+            temp: tArr,
+            at: atArr,
           };
         }
       } catch (error) {
